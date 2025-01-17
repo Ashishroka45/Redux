501e17bf8ad4cf59c25f003abf02e1191f8242cf
Ashish roka <ashishrokaha123@gmail.com>
2025-01-17 06:40:54 +0545

    again

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,25 +1,23 @@
 import React from "react";
-import { useSelector,useDispatch } from "react-redux";
-import {removeToDo} from '../features/todo/todoSlice'
+import { useSelector, useDispatch } from "react-redux";
+import { removeToDo } from "../features/todo/todoSlice";
 
 
-function ToDo(){
-    const todos=useSelector(state => state.todos)
-    const dispatch=useDispatch() 
+function ToDo() {
+  const todos = useSelector(state => state.todos);
 
-     return(
-        <>
-        <div>ToDos</div>
-        {todos.map((todo)=>(
-            <li key={todo.id} >
-                {todo.text}
-                <button
-                onClick={()=>dispatch(removeToDo(todo.id))}
-                >X</button>
-                </li>
-          
-        ))}
-        </>
-    )
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div>ToDos</div>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => dispatch(removeToDo(todo.id))}>X</button>
+        </li>
+      ))}
+    </>
+  );
 }
-export default ToDo
+export default ToDo;
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,34 +1,34 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-const initialStore = {
+const initialState = {
   todos: [{ id: 1, todo: "Hello ashish" }],
 };
 
 export const todoSlice = createSlice({
   name: "todo",
-  initialStore,
+  initialState,
   reducers: {
     addToDo: (state, action) => {
       const todo = {
         id: nanoid(),
-        todo: action.payload.text,
+        text: action.payload.text,
       };
       state.todos.push(todo);
     },
     removeToDo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id != action.payload);
     },
-    updateToDo:(state,action)=>{
+    // updateToDo:(state,action)=>{
 
 
-        // const id=action.payload.id
-        // const text=action.payload.text
-        // state.todos=state.todos.map((todo)=>{
-        //     if(todo.id===id) return todo.text=text
-        //        return todo
-        // })
+    //     const id=action.payload.id
+    //     const text=action.payload.text
+    //     state.todos=state.todos.map((todo)=>{
+    //         if(todo.id===id) return todo.text=text
+    //            return todo
+    //     })
 
-    }
+    // }
   },
 });
 
